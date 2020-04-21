34e5e75ea0eed19c75fd7fc2d6cab857bf58af80
aafmisra <aafmisra@gmail.com>
2020-04-21 16:04:31 -0400

    sumNum recursive

--- sumNum.js
+++ sumNum.js
@@ -2,12 +2,22 @@
 
 //iterative
 
+// function sumNum(num) {
+//   let sum = 0;
+//   for (let i = num; i >= 1; i--) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(sumNum(3));
+
+
+// recursive 
+
 function sumNum(num) {
-  let sum = 0;
-  for (let i = num; i >= 1; i--) {
-    sum += i;
-  }
-  return sum;
+    if(num === 1) return 1;
+    return num + sumNum(num-1)
 }
 
-console.log(sumNum(3));
+console.log(sumNum(3))
