55fee31c1ce387d3e63ce5e0c65e1198d274b859
aafmisra <aafmisra@gmail.com>
2020-05-03 12:11:05 -0400

    binary search python

--- binary-search.py
+++ binary-search.py
@@ -1,6 +1,18 @@
-def binary_search(list, val)
+def binary_search(list, val):
     start = 0
     end = len(list) - 1
-    middle = (start + end) // 2
 
-    
+    while(end > start):
+        middle = (start + end) // 2
+
+        if (val < list[middle]):
+            end = middle - 1
+        elif(val > list[middle]):
+            start = middle + 1
+        else:
+            return middle 
+        
+    return -1 
+
+print(binary_search([1, 2, 3, 5, 8, 10, 13, 17], 10))
+        
